2f78fb0f5182a0679b5fcfed3a9328e27073dc84
stulzq <stulzq@qq.com>
2019-12-16 18:12:25 +0800

    [Add] WIP: Bigdata Encrypt.

---
+++ src/RSAExtensions/EncryptExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAExtensions
+{
+    public static class EncryptExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rsa"></param>
+        /// <param name="data"></param>
+        /// <param name="padding"></param>
+        /// <param name="connChar"></param>
+        /// <returns></returns>
+        public static string EncryptBigData(this RSA rsa,string data, RSAEncryptionPadding padding,char connChar='$')
+        {
+            var keySize = rsa.KeySize;
+
+            var splitLength = 0;
+
+            if (data.Length > keySize)
+            {
+                splitLength = keySize;
+            }
+
+            var sb = new StringBuilder();
+
+            var splitsNumber = Convert.ToInt32(Math.Ceiling(data.Length * 1.0 / splitLength));
+
+            var pointer = 0;
+            for (int i = 0; i < splitsNumber; i++)
+            {
+                var currentStr = data.Length < pointer + splitLength ? data.Substring(pointer, data.Length - pointer) : data.Substring(pointer, splitLength);
+
+                sb.Append(Convert.ToBase64String(rsa.Encrypt(Encoding.UTF8.GetBytes(currentStr), padding)) + connChar);
+                pointer += splitLength;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
